097f3a6f12cd266d682d30e61d230a90b56393d4
unknown <shon@DEV-PC.alifca.local>
2015-09-06 15:00:01 +0400

    Navigation tab bar structure implemented

---
+++ app/src/main/java/com/awok/moshin/awok/Home_Fragment.java
@@ -0,0 +1,32 @@
+package com.awok.moshin.awok;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home_Fragment extends Fragment {
+
+
+    public Home_Fragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+
+
+
+
+
+}
