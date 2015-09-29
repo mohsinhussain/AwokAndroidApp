3839969b0287304f7701aa3d11327b4f033962fc
Mohsin Hussain <phi.me.phi@gmail.com>
2015-09-29 16:59:16 +0400

    internal dialog for login and registration implemented. Dummy Filter screen implemented.

---
+++ app/src/main/java/com/awok/moshin/awok/Activities/FilterActivity.java
@@ -0,0 +1,176 @@
+package com.awok.moshin.awok.Activities;
+
+import android.app.SearchManager;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.awok.moshin.awok.Fragments.HotDealsFragment;
+import com.awok.moshin.awok.R;
+import com.awok.moshin.awok.Util.Constants;
+
+import java.net.URLEncoder;
+
+public class FilterActivity extends AppCompatActivity {
+private DrawerLayout mDrawerLayout;
+
+    ActionBar ab;
+    private String TAG = "Filter Activity";
+//    private String searchString = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ab = getSupportActionBar();
+//        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setTitle("Filter");
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+//        searchString = getIntent().getStringExtra(Constants.SEARCH_FILTER_INTENT);
+//        ab.setTitle(searchString);
+//        URLEncoder.encode(searchString);
+
+//        HotDealsFragment fragment = new HotDealsFragment(searchString, true);
+//        FragmentManager fm = getSupportFragmentManager(); //or getFragmentManager() if you are not using support library.
+//        FragmentTransaction ft = fm.beginTransaction();
+//        ft.replace(R.id.containerLayout, fragment);
+//        ft.commit();
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+
+//                        viewPager.setCurrentItem(0);
+                        mDrawerLayout.closeDrawers();
+
+                        return true;
+                    case R.id.nav_messages:
+//                        viewPager.setCurrentItem(1);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    case R.id.nav_friends:
+//                        viewPager.setCurrentItem(2);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+
+                    case R.id.nav_discussion:
+                        /*Intent i=new Intent(MainActivity.this,ProductDetailsView.class);
+                        startActivity(i);*/
+
+//                        Intent i=new Intent(getApplicationContext(),ProductDetailsView.class);
+//                        startActivity(i);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+
+
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        ImageView closeButton = (ImageView)searchView.findViewById(R.id.search_close_btn);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+  /*      searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                tabLayout.setVisibility(View.VISIBLE);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                tabLayout.setVisibility(View.GONE);
+                return false;
+            }
+        });
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                tabLayout.setVisibility(View.VISIBLE);
+
+                return false;
+            }
+        });
+
+closeButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        tabLayout.setVisibility(View.VISIBLE);
+    }
+});*/
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+
+
+
+               switch (item.getItemId()) {
+            case android.R.id.home:
+//                mDrawerLayout.openDrawer(GravityCompat.START);
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+
+}
--- app/src/main/java/com/awok/moshin/awok/Activities/LauncherActivity.java
+++ app/src/main/java/com/awok/moshin/awok/Activities/LauncherActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,8 +12,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.awok.moshin.awok.R;
 import com.awok.moshin.awok.Util.Constants;
@@ -29,34 +26,34 @@ public class LauncherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launcher);
         mSharedPrefs = getSharedPreferences(Constants.PREFS_NAME, 0);
 
-        // custom dialog
-        final Dialog dialog = new Dialog(this, R.style.AppCompatAlertDialogStyle);
-        dialog.setCancelable(false);
-        dialog.setContentView(R.layout.dialog_enter_mobile);
-        final EditText mobileEditText = (EditText) dialog.findViewById(R.id.mobileEditText);
-        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
-        // if button is clicked, close the custom dialog
-        dialogButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mobileEditText.getText().toString().equalsIgnoreCase("")){
-                    Snackbar.make(findViewById(android.R.id.content), "No network connection available", Snackbar.LENGTH_LONG)
-                            .setActionTextColor(Color.RED)
-                            .show();
-                }
-                else{
-                    SharedPreferences.Editor editor = mSharedPrefs.edit();
-                    editor.putString(Constants.USER_MOBILE_PREFS, mobileEditText.getText().toString());
-                    editor.commit();
-                    Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    dialog.dismiss();
-                }
-            }
-        });
-
-        dialog.show();
+//        // custom dialog
+//        final Dialog dialog = new Dialog(this, R.style.AppCompatAlertDialogStyle);
+//        dialog.setCancelable(false);
+//        dialog.setContentView(R.layout.dialog_input_number);
+//        final EditText mobileEditText = (EditText) dialog.findViewById(R.id.mobileEditText);
+//        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+//        // if button is clicked, close the custom dialog
+//        dialogButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(mobileEditText.getText().toString().equalsIgnoreCase("")){
+//                    Snackbar.make(findViewById(android.R.id.content), "No network connection available", Snackbar.LENGTH_LONG)
+//                            .setActionTextColor(Color.RED)
+//                            .show();
+//                }
+//                else{
+//                    SharedPreferences.Editor editor = mSharedPrefs.edit();
+//                    editor.putString(Constants.USER_MOBILE_PREFS, mobileEditText.getText().toString());
+//                    editor.commit();
+//                    Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                    dialog.dismiss();
+//                }
+//            }
+//        });
+//
+//        dialog.show();
     }
 
     @Override
